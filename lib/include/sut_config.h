90217c8cc8c7cb5dfbc3f24538870a54c23eccf2
solosky <solosky772@qq.com>
2026-05-17 21:27:11 +0800

    c99-port: add project scaffolding, types, allocator, CMake

---
+++ lib/include/sut_config.h
@@ -0,0 +1,7 @@
+#ifndef SUT_CONFIG_H
+#define SUT_CONFIG_H
+
+// Uncomment for platforms with hardware FPU (Cortex-M4F/M7, ESP32-S3, etc.)
+// #define SUT_USE_FLOAT
+
+#endif
---
+++ lib/include/sut_errors.h
@@ -0,0 +1,10 @@
+#ifndef SUT_ERRORS_H
+#define SUT_ERRORS_H
+
+#define SUT_OK          0
+#define SUT_ERR_NOMEM  -1
+#define SUT_ERR_FULL   -2
+#define SUT_ERR_EMPTY  -3
+#define SUT_ERR_INVAL  -4
+
+#endif
